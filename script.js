7e0cbdb7990e1349a53a0a2e0b0defd12dc8edc9
phogatheena <109434462+phogatheena@users.noreply.github.com>
2023-08-13 00:56:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
+	 if (num === 0) {
+    return '0';
+  }
+
+  let a= '';
+  while (num > 0) {
+    a = (num % 2) + binary;
+    num = num/ 2;
+  }
+
+  return binary;
   
 }
 
